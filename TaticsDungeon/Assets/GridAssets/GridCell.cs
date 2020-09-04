7cd3143963632c3476fe5d4c7c541620e8897d93
Morgan Payette <morgan_payette@yahoo.ca>
2020-09-04 13:02:03 -0600

    Fixed highlights on elevated services

--- TaticsDungeon/Assets/GridAssets/GridCell.cs
+++ TaticsDungeon/Assets/GridAssets/GridCell.cs
@@ -99,7 +99,7 @@ namespace PrototypeGame
             highlighted = true;
             if (highlightEffect != null)
                 Destroy(highlightEffect);
-            highlightEffect = Instantiate(effect, transform.position, transform.rotation);
+            highlightEffect = Instantiate(effect, new Vector3(transform.position.x, transform.position.y + height * GridMetrics.heightIncrement, transform.position.z), transform.rotation);
             highlightEffect.transform.SetParent(transform);
         }
     }
