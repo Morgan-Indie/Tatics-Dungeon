a8ef480353ea18bc1ae29ff8f54972f1b10e1680
BYZ007 <44309855+BYZ007@users.noreply.github.com>
2020-10-16 15:01:06 -0600

    Broken Update
    Broken Update

---
+++ TaticsDungeon/Assets/Scripts/CharacterLocation.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PrototypeGame
+{
+    public class CharacterLocation : MonoBehaviour
+    {
+        [Header("Required")]
+        public GridMapAdapter mapAdapter;
+
+        public GridCell currentCell
+        {
+            get { return mapAdapter.GetCellByPos(transform.position); }
+        }
+
+        public IntVector2 currentIndex
+        {
+            get{ return currentCell.index; }
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            mapAdapter = GridManager.Instance.mapAdapter;
+        }
+    }
+}
+
